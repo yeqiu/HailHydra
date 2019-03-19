6feb19888a98777551a03f28ef294ad886b48e7b
叶秋 <ypf@git.com>
2019-03-19 18:17:00 +0800

    升级android studio 3.0

--- hydrautils/src/main/java/com/yeqiu/hydrautils/utils/ResourceUtil.java
+++ hydrautils/src/main/java/com/yeqiu/hydrautils/utils/ResourceUtil.java
@@ -2,6 +2,7 @@ package com.yeqiu.hydrautils.utils;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 
 import com.yeqiu.hydrautils.HydraUtilsManager;
 
@@ -16,30 +17,35 @@ public class ResourceUtil {
 
     /**
      * 获得字符串
+     *
      * @param strId 字符串id
      * @return 字符串
      */
-    public static String getString( int strId){
+    public static String getString(int strId) {
         Context context = HydraUtilsManager.getInstance().getContext();
         return context.getResources().getString(strId);
     }
+
     /**
      * 获得颜色
+     *
      * @param colorId 颜色id
      * @return 颜色
      */
-    public static int getColor(int colorId){
+    public static int getColor(int colorId) {
         Context context = HydraUtilsManager.getInstance().getContext();
-        return context.getResources().getColor(colorId);
+
+        return ContextCompat.getColor(context, colorId);
     }
 
     /**
      * 获得Drawable
+     *
      * @param drawableId Drawable的id
      * @return Drawable
      */
-    public static Drawable getDrawable(int drawableId){
+    public static Drawable getDrawable(int drawableId) {
         Context context = HydraUtilsManager.getInstance().getContext();
-        return context.getResources().getDrawable(drawableId);
+        return ContextCompat.getDrawable(context, drawableId);
     }
 }
