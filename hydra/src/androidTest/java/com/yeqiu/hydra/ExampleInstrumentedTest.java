f03351e8b931451a9d34fdfa2b278d93516d36fd
ypf <yeqiu0000@gmail.com>
2019-07-25 11:03:26 +0800

    升级到新版andorid sutdio

--- hydra/src/androidTest/java/com/yeqiu/hydra/ExampleInstrumentedTest.java
+++ hydra/src/androidTest/java/com/yeqiu/hydra/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
-package com.yeqiu.hydrautils;
+package com.yeqiu.hydra;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
@@ -10,17 +11,17 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumentation test, which will execute on an Android device.
+ * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.yeqiu.hydrautils.test", appContext.getPackageName());
+        assertEquals("com.yeqiu.hydra.test", appContext.getPackageName());
     }
 }
